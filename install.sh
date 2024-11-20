a93dfd9ee5b4407568970b344118b953828b8eee
Robin Rebers <robin.rebers@gmail.com>
2024-11-20 16:25:09 +0100

    I am an idiot

--- install.sh
+++ install.sh
@@ -7,15 +7,17 @@ mkdir ~/.config/nvim
 mkdir ~/.config/yazi
 mkdir ~/.config/waybar
 # neovim dotfiles
-rm -rf ~/.config/nvim/init.lua && ln -s ./nvim/init.lua ~/.config/nvim/init.lua
-rm -rf ~/.config/nvim/lazy-lock.json && ln -s ./nvim/lazy-lock.lua ~/.config/nvim/lazy-lock.lua
-rm -rf ~/.config/nvim/plugins.lua && ln -s ./nvim/plugins.lua ~/.config/nvim/plugins.lua
+rm -rf ~/.config/nvim/init.lua && ln -s ~/projects/archdots/nvim/init.lua ~/.config/nvim/init.lua
+rm -rf ~/.config/nvim/lazy-lock.json && ln -s ~/projects/archdots/nvim/lazy-lock.lua ~/.config/nvim/lazy-lock.lua
+rm -rf ~/.config/nvim/plugins.lua && ln -s ~/projects/archdots/nvim/plugins.lua ~/.config/nvim/plugins.lua
 # hyprland and hyprtools dotfiles
-rm -rf ~/.config/hypr/hyprland.conf && ln -s ./hypr/hyprland.conf ~/.config/hypr/hyprland.conf
-rm -rf ~/.config/hypr/hyprpaper.conf && ln -s ./hypr/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
-rm -rf ~/.config/hypr/hypridle.conf && ln -s ./hypr/hypridle.conf ~/.config/hypr/hypridle.conf
+rm -rf ~/.config/hypr/hyprland.conf && ln -s ~/projects/archdots/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
+rm -rf ~/.config/hypr/hyprpaper.conf && ln -s ~/projects/archdots/hypr/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
+rm -rf ~/.config/hypr/hypridle.conf && ln -s ~/projects/archdots/hypr/hypridle.conf ~/.config/hypr/hypridle.conf
 # yazi dotfiles
-rm -rf ~/.config/yazi/yazi.toml && ln -s ./yazi/yazi.toml ~/.config/yazi/yazi.toml
+rm -rf ~/.config/yazi/yazi.toml && ln -s ~/projects/archdots/yazi/yazi.toml ~/.config/yazi/yazi.toml
 # waybar dotfiles
-rm -rf ~/.config/waybar/config && ln -s ./waybar/config ~/.config/waybar/config
-rm -rf ~/.config/waybar/style.css && ln -s ./waybar/style.css ~/.config/waybar/style.css
+rm -rf ~/.config/waybar/config && ln -s ~/projects/archdots/waybar/config ~/.config/waybar/config
+rm -rf ~/.config/waybar/style.css && ln -s ~/projects/archdots/waybar/style.css ~/.config/waybar/style.css
+# bashrc dotfile
+rm -rf ~/.bashrc && ln -s ~/projects.archdots/bash/.bashrc ~/.bashrc
