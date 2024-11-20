4ca8c59994d7472597feab6636590266396c6fca
Robin Rebers <robin.rebers@gmail.com>
2024-11-20 13:41:19 +0100

    Fixed install script, missing mkdir

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@ echo 'Starting install script'
 echo 'Install required applications'
 yay -Syu fzf rg neovim yazi pavucontrol audio-recorder clipboard e2fsprogs f2fs-tools hyprcursor hyprpaper hypridle hyprutils waybar mako power-profile-daemon rofi sudo ttf-font-awesome ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-common ttf-hack ttf-liberation vim yt-dlp
 echo 'Creating symlinks for dotfiles'
+mkdir ~/.config/nvim
+mkdir ~/.config/yazi
+mkdir ~/.config/waybar
 # neovim dotfiles
 rm -rf ~/.config/nvim/init.lua && ln -s ./nvim/init.lua ~/.config/nvim/init.lua
 rm -rf ~/.config/nvim/lazy-lock.json && ln -s ./nvim/lazy-lock.lua ~/.config/nvim/lazy-lock.lua
