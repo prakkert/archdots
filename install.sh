2cba1f8474f163a3735c40f9fb1bcc7bec5f2bdf
Robin Rebers <robin.rebers@gmail.com>
2024-11-27 00:56:59 +0100

    Restructured install.sh, added nvim plugins, moved wallpapers

--- install.sh
+++ install.sh
@@ -1,31 +1,54 @@
 #!/bin/bash
-echo 'Starting install script'
-echo 'Install required applications'
-yay -Syu fzf ripgrep neovim yazi pavucontrol audio-recorder clipboard e2fsprogs f2fs-tools hyprcursor hyprpaper hypridle hyprutils waybar mako power-profiles-daemon rofi-wayland sudo ttf-font-awesome ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-common ttf-hack ttf-liberation vim yt-dlp
-echo 'Creating symlinks for dotfiles'
-mkdir ~/.config/nvim
-mkdir ~/.config/yazi
-mkdir ~/.config/waybar
-mkdir ~/.config/mako
-mkdir ~/.config/hypr/conf
-# neovim dotfiles
-rm -rf ~/.config/nvim/init.lua && ln -s ~/projects/archdots/nvim/init.lua ~/.config/nvim/init.lua
-rm -rf ~/.config/nvim/lazy-lock.json && ln -s ~/projects/archdots/nvim/lazy-lock.lua ~/.config/nvim/lazy-lock.lua
-rm -rf ~/.config/nvim/plugins.lua && ln -s ~/projects/archdots/nvim/plugins.lua ~/.config/nvim/plugins.lua
+echo -e 'Starting install script\n\n'
+# echo 'Install required applications'
+# yay -Syu fzf ripgrep neovim yazi pavucontrol audio-recorder clipboard e2fsprogs f2fs-tools hyprcursor hyprpaper hypridle hyprutils waybar mako power-profiles-daemon rofi-wayland sudo ttf-font-awesome ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-common ttf-hack ttf-liberation vim yt-dlp
+echo -e 'Deleting config folders and .bashrc\n\n'
+rm -rf ~/.config/hypr/
+rm -rf ~/.config/mako/
+rm -rf ~/.config/nvim/
+rm -rf ~/.config/rofi/
+rm -rf ~/.config/waybar/
+rm -rf ~/.config/yazi/
+rm -rf ~/.bashrc 
+echo -e 'Creating directories for config files\n\n'
+mkdir ~/.config/hypr/
+mkdir ~/.config/hypr/conf/
+mkdir ~/.config/mako/
+mkdir ~/.config/nvim/
+mkdir ~/.config/nvim/lua/
+mkdir ~/.config/nvim/lua/plugins/
+mkdir ~/.config/rofi/
+mkdir ~/.config/waybar/
+mkdir ~/.config/yazi/
+echo -e 'Creating symlinks for dotfiles\n\n'
+### softlink format: ln -s actual_file softlink_file
+# .bashrc dotfile
+ln -s ~/projects/archdots/bash/.bashrc ~/.bashrc
 # hyprland and hyprtools dotfiles
-rm -rf ~/.config/hypr/hyprland.conf && ln -s ~/projects/archdots/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
-rm -rf ~/.config/hypr/conf/monitor.conf && ln -s ~/projects/archdots/hypr/conf/monitor.conf ~/.config/hypr/config/monitor.conf
-rm -rf ~/.config/hypr/hyprpaper.conf && ln -s ~/projects/archdots/hypr/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
-rm -rf ~/.config/hypr/hypridle.conf && ln -s ~/projects/archdots/hypr/hypridle.conf ~/.config/hypr/hypridle.conf
-# yazi dotfiles
-rm -rf ~/.config/yazi/yazi.toml && ln -s ~/projects/archdots/yazi/yazi.toml ~/.config/yazi/yazi.toml
-# waybar dotfiles
-rm -rf ~/.config/waybar/config && ln -s ~/projects/archdots/waybar/config ~/.config/waybar/config
-rm -rf ~/.config/waybar/style.css && ln -s ~/projects/archdots/waybar/style.css ~/.config/waybar/style.css
-# bashrc dotfile
-rm -rf ~/.bashrc && ln -s ~/projects/archdots/bash/.bashrc ~/.bashrc
-rm -rf ~/.config/kitty/current-theme.conf && ln -s ~/projects/archdots/kitty/current-theme.conf ~/.config/kitty/current-theme.conf
+ln -s ~/projects/archdots/hypr/hyprland.conf ~/.config/hypr/hyprland.conf
+ln -s ~/projects/archdots/hypr/conf/monitor.conf ~/.config/hypr/conf/monitor.conf
+ln -s ~/projects/archdots/hypr/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
+ln -s ~/projects/archdots/hypr/hypridle.conf ~/.config/hypr/hypridle.conf
+# kitty dotfiles
+ln -s ~/projects/archdots/kitty/kitty.conf ~/.config/kitty/kitty.conf
+ln -s ~/projects/archdots/kitty/current-theme.conf ~/.config/kitty/current-theme.conf
 # mako dotfiles
-rm -rf ~/.config/mako/config && ln -s ~/projects/archdots/mako/config ~/.config/mako/config
+ln -s ~/projects/archdots/mako/config ~/.config/mako/config
+# neovim dotfiles
+ln -s ~/projects/archdots/nvim/init.lua ~/.config/nvim/init.lua
+ln -s ~/projects/archdots/nvim/lazy-lock.lua ~/.config/nvim/lazy-lock.lua
+ln -s ~/projects/archdots/nvim/lua/plugins.lua ~/.config/nvim/lua/plugins.lua
+ln -s ~/projects/archdots/nvim/lua/plugins/catppuccin.lua ~/.config/nvim/lua/plugins/catppuccin.lua
+ln -s ~/projects/archdots/nvim/lua/plugins/lsp-config.lua ~/.config/nvim/lua/plugins/lsp-config.lua
+ln -s ~/projects/archdots/nvim/lua/plugins/lualine.lua ~/.config/nvim/lua/plugins/lualine.lua
+ln -s ~/projects/archdots/nvim/lua/plugins/nvim-neo-tree.lua ~/.config/nvim/lua/plugins/nvim-neo-tree.lua
+ln -s ~/projects/archdots/nvim/lua/plugins/nvim-treesitter.lua ~/.config/nvim/lua/plugins/nvim-treesitter.lua
+ln -s ~/projects/archdots/nvim/lua/plugins/telescope.lua ~/.config/nvim/lua/plugins/telescope.lua
+ln -s ~/projects/archdots/nvim/lua/plugins/vim-be-good.lua ~/.config/nvim/lua/plugins/vim-be-good.lua
 # rofi dotfiles
-rm -rf ~/.config/rofi/config.rasi && ln -s ~/projects/archdots/rofi/config.rasi ~/.config/rofi/config.rasi
+ln -s ~/projects/archdots/rofi/config.rasi ~/.config/rofi/config.rasi
+# waybar dotfiles
+ln -s ~/projects/archdots/waybar/config ~/.config/waybar/config
+ln -s ~/projects/archdots/waybar/style.css ~/.config/waybar/style.css
+# yazi dotfiles
+ln -s ~/projects/archdots/yazi/yazi.toml ~/.config/yazi/yazi.toml
--- nvim/init.lua
+++ nvim/init.lua
@@ -6,7 +6,6 @@ vim.cmd("set number")
 vim.cmd("set relativenumber")
 vim.g.mapleader = " "
 
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -18,51 +17,9 @@ if not vim.loop.fs_stat(lazypath) then
     lazypath,
   })
 end
+
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  { 
-    "nvim-telescope/telescope.nvim", tag = "0.1.8", dependencies = { "nvim-lua/plenary.nvim" }
-  },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", 
-      "MunifTanjim/nui.nvim",
-    },
-  },
-  {"ThePrimeagen/vim-be-good"}
-}
 local opts = {}
 
-require("lazy").setup(plugins, opts)
-
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-
-vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>")
-
-require("neo-tree").setup {
-  filesystem = {
-    filtered_items = {
-      visible = true,
-      hide_dotfiles = false,
-      hide_gitignored = true,
-    }
-  }
-}
-
-local config = require("nvim-treesitter.configs")
-config.setup({
-  ensure_installed = {"lua", "python", "hcl", "bash"},
-  highlight = { enable = true },
-  indent = { enable = true }
-  })
-
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
+require("lazy").setup("plugins")
